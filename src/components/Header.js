1976838e09798a2905ce892b9ccd5cd9cba38fda
CharanKumar-1 <118291501+CharanKumar-1@users.noreply.github.com>
2024-05-27 15:40:02 +0530

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img 
+                src="C:/Users/HP/Desktop/Downloads/tree-736885_1280.jpg"
+                className="header--image"
+                alt=""
+            />
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">React Course - Project 3</h4>
+        </header>
+    )
+}
